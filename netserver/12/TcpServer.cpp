60528d834cbfe4913e786e27e39e0f868e86b49e
xiety <qyyzxty@icloud.com>
2025-03-26 23:47:05 +0800

    8-4 封装Acceptor

--- netserver/12/TcpServer.cpp
+++ netserver/12/TcpServer.cpp
@@ -4,18 +4,7 @@
 
 TcpServer::TcpServer(const std::string& ip, uint16_t port)
 {
-    Socket* servsock = new Socket(createnonblocking());
-    InetAddress servaddr(ip.c_str(), port);
-    servsock->setreuseaddr(true);
-    servsock->settcpnodelay(true);
-    servsock->setreuseport(true);
-    servsock->setkeepalive(true);
-    servsock->bind(servaddr);
-    servsock->listen();
-
-    Channel* servchannel = new Channel(&loop_, servsock->fd());
-    servchannel->setreadcallback(std::bind(&Channel::newconnection, servchannel, servsock));
-    servchannel->enablereading();
+    acceptor_ = new Acceptor(&loop_, ip, port);
 }
 
 TcpServer::~TcpServer()
