43ec868dd591a238c2561255266618974c161d88
xiety <qyyzxty@icloud.com>
2025-04-06 17:46:50 +0800

    9-9 begin

---
+++ netserver/23/TcpServer.cpp
@@ -0,0 +1,84 @@
+#include "TcpServer.h"
+
+TcpServer::TcpServer(const std::string &ip,const uint16_t port)
+{
+    acceptor_=new Acceptor(&loop_,ip,port);
+    acceptor_->setnewconnectioncb(std::bind(&TcpServer::newconnection,this,std::placeholders::_1));
+    loop_.setepolltimeoutcallback(std::bind(&TcpServer::epolltimeout,this,std::placeholders::_1));
+}
+
+TcpServer::~TcpServer()
+{
+    delete acceptor_;
+
+    // 释放全部的Connection对象。
+    for (auto &aa:conns_)
+    {
+        delete aa.second;
+    }
+}
+
+// 运行事件循环。
+void TcpServer::start()          
+{
+    loop_.run();
+}
+
+// 处理新客户端连接请求。
+void TcpServer::newconnection(Socket* clientsock)
+{
+    Connection *conn=new Connection(&loop_,clientsock);   
+    conn->setclosecallback(std::bind(&TcpServer::closeconnection,this,std::placeholders::_1));
+    conn->seterrorcallback(std::bind(&TcpServer::errorconnection,this,std::placeholders::_1));
+    conn->setonmessagecallback(std::bind(&TcpServer::onmessage,this,std::placeholders::_1,std::placeholders::_2));
+    conn->setsendcompletecallback(std::bind(&TcpServer::sendcomplete,this,std::placeholders::_1));
+    printf ("new connection(fd=%d,ip=%s,port=%d) ok.\n",conn->fd(),conn->ip().c_str(),conn->port());
+
+    conns_[conn->fd()]=conn;            // 把conn存放map容器中。
+}
+
+ // 关闭客户端的连接，在Connection类中回调此函数。 
+ void TcpServer::closeconnection(Connection *conn)
+ {
+    printf("client(eventfd=%d) disconnected.\n",conn->fd());
+    // close(conn->fd());            // 关闭客户端的fd。
+    conns_.erase(conn->fd());   // 从map中删除conn。
+    delete conn;
+ }
+
+// 客户端的连接错误，在Connection类中回调此函数。
+void TcpServer::errorconnection(Connection *conn)
+{
+    printf("client(eventfd=%d) error.\n",conn->fd());
+    // close(conn->fd());            // 关闭客户端的fd。
+    conns_.erase(conn->fd());   // 从map中删除conn。
+    delete conn;
+}
+
+void TcpServer::onmessage(Connection* conn, std::string message)
+{
+    // 假设在这里经过复杂运算
+    message = "reply: " + message;
+
+    // 构造回复消息
+    int len = message.size();
+    std::string tmpbuf((char*)&len, 4);
+    tmpbuf.append(message);
+
+    // 发送回复消息
+    conn->send(tmpbuf.data(), tmpbuf.size());
+}
+
+void TcpServer::sendcomplete(Connection* conn)
+{
+    printf("send complete\n");
+
+    // 根据业务需求增加代码
+}
+
+void TcpServer::epolltimeout(EventLoop* loop)
+{
+    printf("epoll_wait() timeout\n");
+
+    // 根据业务需求增加代码
+}
---
+++ netserver/23/client.cpp
@@ -0,0 +1,70 @@
+// 网络通讯的客户端程序。
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("usage:./client ip port\n"); 
+        printf("example:./client 192.168.150.128 5085\n\n"); 
+        return -1;
+    }
+
+    int sockfd;
+    struct sockaddr_in servaddr;
+    char buf[1024];
+ 
+    if ((sockfd=socket(AF_INET,SOCK_STREAM,0))<0) 
+    {
+        printf("socket() failed.\n");
+        return -1;
+    }
+    
+    memset(&servaddr,0,sizeof(servaddr));
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_port=htons(atoi(argv[2]));
+    servaddr.sin_addr.s_addr=inet_addr(argv[1]);
+
+    if (connect(sockfd, (struct sockaddr *)&servaddr,sizeof(servaddr)) != 0)
+    {
+        printf("connect(%s:%s) failed.\n",argv[1],argv[2]);
+        close(sockfd);
+        return -1;
+    }
+
+    printf("connect ok.\n");
+
+    for (int ii=0;ii<2;ii++)
+    {
+        // 从命令行输入内容。
+        memset(buf,0,sizeof(buf));
+        sprintf(buf, "这是第%d个超级女生", ii);
+
+        char tmpbuf[1024];
+        memset(tmpbuf, 0, sizeof(tmpbuf));
+        int len = strlen(buf);
+        memcpy(tmpbuf, &len, 4);
+        memcpy(tmpbuf+4, buf, len);
+
+        send(sockfd,tmpbuf,len+4,0);
+    }
+
+    for (int ii=0;ii<2;ii++)
+    {
+        int len;
+        recv(sockfd, &len, 4, 0);
+
+        memset(buf,0,sizeof(buf));
+        recv(sockfd,buf,len,0);
+
+        printf("recv:%s\n",buf);
+    }
+} 
