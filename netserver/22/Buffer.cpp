f44dc20fb5d20ad4d0ce287568eb03e28bd30e9f
xiety <qyyzxty@icloud.com>
2025-04-06 14:06:13 +0800

    9-8 begin

---
+++ netserver/22/Buffer.cpp
@@ -0,0 +1,32 @@
+#include "Buffer.h"
+
+Buffer::Buffer()
+{}
+
+Buffer::~Buffer()
+{}
+
+void Buffer::append(const char* data, size_t size)
+{
+    buf_.append(data, size);
+}
+
+void Buffer::erase(size_t pos, size_t nn)
+{
+    buf_.erase(pos, nn);
+}
+
+size_t Buffer::size()
+{
+    return buf_.size();
+}
+
+void Buffer::clear()
+{
+    buf_.clear();
+}
+
+const char* Buffer::data()
+{
+    return buf_.data();
+}
